64e83c443919d51d5ef124aaa7d6facaaa829def
Faiyaz Huq <faiyaz.huq@mail.utoronto.ca>
2018-03-19 17:01:36 -0400

    starter files for mobile

---
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.example.android.justjava.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
